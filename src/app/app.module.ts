b2f253e46a3fc008f8fe87fd65d28a10473916d1
Eduardo Augusto <a226804875@fumec.edu.br>
2018-10-06 00:30:10 -0300

    Configuração Inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Router } from '@angular/router';
+
+import { ROUTES } from './app.routes';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LeftSideMenuComponent } from './left-side-menu/left-side-menu.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { RightSideMenuComponent } from './right-side-menu/right-side-menu.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LeftSideMenuComponent,
+    HomeComponent,
+    FooterComponent,
+    RightSideMenuComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
